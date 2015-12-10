59f5b325d590183a767174de5bfdf18eda5f1234
loztzero <support@istoreline.com>
2015-12-10 20:21:14 +0700

    commit hotel reigister

--- document/create.sql
+++ document/create.sql
@@ -5,7 +5,7 @@
 # Project name:                                                          #
 # Author:                                                                #
 # Script type:           Database creation script                        #
-# Created on:            2015-11-28 20:00                                #
+# Created on:            2015-12-10 20:19                                #
 # ---------------------------------------------------------------------- #
 
 
@@ -21,9 +21,11 @@ CREATE TABLE `MST020` (
     `id` VARCHAR(100) NOT NULL,
     `hotel_id` VARCHAR(100) NOT NULL COMMENT 'nama company',
     `hotel_name` VARCHAR(100) NOT NULL COMMENT 'alamat',
-    `postcode` VARCHAR(15) COMMENT 'kode pos',
+    `star` INTEGER(5) NOT NULL,
+    `address` VARCHAR(1024) NOT NULL,
     `mst002_id` VARCHAR(100) NOT NULL COMMENT 'negara',
     `mst003_id` VARCHAR(100) NOT NULL COMMENT 'kota',
+    `postcode` VARCHAR(15) COMMENT 'kode pos',
     `phone_number` VARCHAR(40) NOT NULL COMMENT 'no telepon',
     `fax_number` VARCHAR(40) COMMENT 'nomor fax',
     `landmark_name` VARCHAR(1024),
@@ -44,6 +46,48 @@ CREATE TABLE `MST020` (
 ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;;
 
 # ---------------------------------------------------------------------- #
+# Add table "MST021"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `MST021` (
+    `id` VARCHAR(100) NOT NULL,
+    `mst020_id` VARCHAR(100) NOT NULL COMMENT 'nama company',
+    `line_number` VARCHAR(100) NOT NULL COMMENT 'alamat',
+    `pict` VARCHAR(100) NOT NULL COMMENT 'kode pos',
+    `description` VARCHAR(1024),
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    CONSTRAINT `PK_MST021` PRIMARY KEY (`id`),
+    CONSTRAINT `TUC_MST021_1` UNIQUE (`mst020_id`, `line_number`)
+)
+ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;;
+
+# ---------------------------------------------------------------------- #
+# Add table "MST022"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `MST022` (
+    `id` VARCHAR(100) NOT NULL,
+    `mst020_id` VARCHAR(100) NOT NULL COMMENT 'nama company',
+    `room_id` VARCHAR(100) NOT NULL COMMENT 'alamat',
+    `from_date` DATE NOT NULL,
+    `end_date` DATE,
+    `room_name` VARCHAR(100) NOT NULL COMMENT 'kode pos',
+    `bed_type` VARCHAR(100) NOT NULL COMMENT 'negara',
+    `breakfast_no` INTEGER(5) NOT NULL COMMENT 'ada berapa org breakfast',
+    `allotment` INTEGER COMMENT 'kapasitas',
+    `cut_off` INTEGER(5) COMMENT 'cutt off dalam hari',
+    `room_desc` VARCHAR(1024) NOT NULL COMMENT 'kota',
+    `mst004_id` VARCHAR(100) NOT NULL,
+    `price` DOUBLE(30,10) NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    CONSTRAINT `PK_MST022` PRIMARY KEY (`id`),
+    CONSTRAINT `TUC_MST022_1` UNIQUE (`mst020_id`, `room_id`, `end_date`)
+)
+ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;;
+
+# ---------------------------------------------------------------------- #
 # Foreign key constraints                                                #
 # ---------------------------------------------------------------------- #
 
@@ -57,4 +101,13 @@ ALTER TABLE `MST020` ADD CONSTRAINT `MST003_MST020`
     FOREIGN KEY (`mst003_id`) REFERENCES `MST003` (`id`);
 
 ALTER TABLE `MST020` ADD CONSTRAINT `MST004_MST020` 
+    FOREIGN KEY (`mst004_id`) REFERENCES `MST004` (`id`);
+
+ALTER TABLE `MST021` ADD CONSTRAINT `MST020_MST021` 
+    FOREIGN KEY (`mst020_id`) REFERENCES `MST020` (`id`);
+
+ALTER TABLE `MST022` ADD CONSTRAINT `MST020_MST022` 
+    FOREIGN KEY (`mst020_id`) REFERENCES `MST020` (`id`);
+
+ALTER TABLE `MST022` ADD CONSTRAINT `MST004_MST022` 
     FOREIGN KEY (`mst004_id`) REFERENCES `MST004` (`id`);
--- document/drop.sql
+++ document/drop.sql
@@ -5,7 +5,7 @@
 # Project name:                                                          #
 # Author:                                                                #
 # Script type:           Database drop script                            #
-# Created on:            2015-11-28 20:00                                #
+# Created on:            2015-12-10 20:19                                #
 # ---------------------------------------------------------------------- #
 
 
@@ -20,6 +20,40 @@ ALTER TABLE `MST020` DROP FOREIGN KEY `MST002_MST020`;
 ALTER TABLE `MST020` DROP FOREIGN KEY `MST003_MST020`;
 
 ALTER TABLE `MST020` DROP FOREIGN KEY `MST004_MST020`;
+
+ALTER TABLE `MST021` DROP FOREIGN KEY `MST020_MST021`;
+
+ALTER TABLE `MST022` DROP FOREIGN KEY `MST020_MST022`;
+
+ALTER TABLE `MST022` DROP FOREIGN KEY `MST004_MST022`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "MST022"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `MST022` DROP PRIMARY KEY;
+
+DROP INDEX `TUC_MST022_1` ON `MST022`;
+
+# Drop table #
+
+DROP TABLE `MST022`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "MST021"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `MST021` DROP PRIMARY KEY;
+
+DROP INDEX `TUC_MST021_1` ON `MST021`;
+
+# Drop table #
+
+DROP TABLE `MST021`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "MST020"                                                    #
