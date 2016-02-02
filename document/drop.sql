1a4d5accb426fb95b9f2b55d9aff89cb302b392b
loztzero <support@istoreline.com>
2016-02-02 20:36:16 +0700

    table order booking

--- document/drop.sql
+++ document/drop.sql
@@ -5,7 +5,7 @@
 # Project name:                                                          #
 # Author:                                                                #
 # Script type:           Database drop script                            #
-# Created on:            2016-01-26 21:32                                #
+# Created on:            2016-02-02 20:35                                #
 # ---------------------------------------------------------------------- #
 
 
@@ -17,11 +17,13 @@ ALTER TABLE `TRX010` DROP FOREIGN KEY `MST001_TRX010`;
 
 ALTER TABLE `TRX010` DROP FOREIGN KEY `MST004_TRX010`;
 
-ALTER TABLE `TRX013` DROP FOREIGN KEY `MST020_TRX013`;
-
 ALTER TABLE `TRX013` DROP FOREIGN KEY `TRX011_TRX013`;
 
 ALTER TABLE `TRX012` DROP FOREIGN KEY `TRX010_TRX012`;
+
+ALTER TABLE `BLNC001` DROP FOREIGN KEY `MST001_BLNC001`;
+
+ALTER TABLE `BLNC001` DROP FOREIGN KEY `MST004_BLNC001`;
 
 ALTER TABLE `BLNC003` DROP FOREIGN KEY `BLNC001_BLNC003`;
 
@@ -29,25 +31,15 @@ ALTER TABLE `TRX011` DROP FOREIGN KEY `TRX010_TRX011`;
 
 ALTER TABLE `TRX011` DROP FOREIGN KEY `MST020_TRX011`;
 
-ALTER TABLE `BLNC004` DROP FOREIGN KEY `BLNC002_BLNC004`;
+ALTER TABLE `TRX011` DROP FOREIGN KEY `MST023_TRX011`;
 
 ALTER TABLE `BLNC002` DROP FOREIGN KEY `BLNC001_BLNC002`;
 
 ALTER TABLE `BLNC002` DROP FOREIGN KEY `MST020_BLNC002`;
 
-# ---------------------------------------------------------------------- #
-# Drop table "BLNC004"                                                   #
-# ---------------------------------------------------------------------- #
+ALTER TABLE `BLNC002` DROP FOREIGN KEY `MST023_BLNC002`;
 
-# Drop constraints #
-
-ALTER TABLE `BLNC004` DROP PRIMARY KEY;
-
-DROP INDEX `TUC_BLNC004_1` ON `BLNC004`;
-
-# Drop table #
-
-DROP TABLE `BLNC004`;
+ALTER TABLE `BLNC004` DROP FOREIGN KEY `BLNC002_BLNC004`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "TRX013"                                                    #
@@ -62,6 +54,20 @@ DROP INDEX `TUC_TRX013_1` ON `TRX013`;
 # Drop table #
 
 DROP TABLE `TRX013`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "BLNC004"                                                   #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `BLNC004` DROP PRIMARY KEY;
+
+DROP INDEX `TUC_BLNC004_1` ON `BLNC004`;
+
+# Drop table #
+
+DROP TABLE `BLNC004`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "BLNC002"                                                   #
