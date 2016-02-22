6d676f6b2cd84655401acec179e32e88fc0c1442
loztzero <support@istoreline.com>
2016-02-22 12:01:26 +0700

    revisi sp
    sp revision

--- document/spSearchDetailHotel.sql
+++ document/spSearchDetailHotel.sql
@@ -8,57 +8,61 @@ BEGIN
 
     -- ambil semua room yang possible
      INSERT INTO TEMP003
-        (SESSION_ID,MST022_ID,MST023_ID)
-     SELECT pSessionId,C.ID,D.ID
+        (session_id, mst022_id, mst023_id)
+     SELECT pSessionId,C.id,D.id
      FROM MST022 C
-     INNER JOIN MST023 D ON D.ID = C.MST023_ID
-     WHERE  C.NUM_ADULTS >= 0
-     AND C.ALLOTMENT - C.USED_ALLOTMENT >= pAllotment
+     INNER JOIN MST023 D ON D.id = C.mst023_id
+     WHERE  C.num_adults >= 0
+     AND C.allotment - C.USED_allotment >= pAllotment
      AND
       (
-        C.FROM_DATE >= STR_TO_DATE(pFromDate, '%m/%d/%Y')
+        C.from_date >= STR_TO_DATE(pFromDate, '%d-%m-%Y')
         OR
-        C.END_DATE >= STR_TO_DATE(pFromDate, '%m/%d/%Y')
+        C.end_date >= STR_TO_DATE(pFromDate, '%d-%m-%Y')
       )
 
      AND
       (
-        C.END_DATE <= STR_TO_DATE(pEndDate, '%m/%d/%Y')
+        C.end_date <= STR_TO_DATE(pEndDate, '%d-%m-%Y')
         OR
-        C.FROM_DATE <= STR_TO_DATE(pEndDate, '%m/%d/%Y')
+        C.from_date <= STR_TO_DATE(pEndDate, '%d-%m-%Y')
       )
 
-     AND STR_TO_DATE(pFromDate, '%m/%d/%Y') >=
+     AND STR_TO_DATE(pFromDate, '%d-%m-%Y') >=
       (
-        SELECT MIN(AB.FROM_DATE) FROM MST022 AB WHERE AB.MST020_ID = C.MST020_ID
+        SELECT MIN(AB.from_date) FROM MST022 AB WHERE AB.mst020_id = C.mst020_id
       )
 
-     AND STR_TO_DATE(pEndDate, '%m/%d/%Y') <=
+     AND STR_TO_DATE(pEndDate, '%d-%m-%Y') <=
       (
-        SELECT MAX(AC.END_DATE) FROM MST022 AC WHERE AC.MST020_ID = C.MST020_ID
-      ) AND C.MST020_ID = pHotelId;
+        SELECT MAX(AC.end_date) FROM MST022 AC WHERE AC.mst020_id = C.mst020_id
+      ) AND C.mst020_id = pHotelId;
 
       -- beri nomor count per kamar
       INSERT INTO TEMP004
-        (SESSION_ID,mst023_id,record)
+        (session_id, mst023_id, record)
       SELECT 
-        pSessionId,MST023_ID,COUNT(*)
+        pSessionId,mst023_id,COUNT(*)
        FROM TEMP003
-       GROUP BY MST023_ID;
+       GROUP BY mst023_id;
 
        -- query sesuai kebutuhan
-       SELECT B.mst020_id,C.room_name,B.num_adults,B.num_child,B.num_breakfast,
-             B.from_date,B.end_date,B.net_fee,B.net,B.cancel_fee_flag,B.cancel_fee_val,
-             B.allotment-B.used_allotment,B.comm_value,B.cut_off,B.bed_type,
-             CASe WHEN UPPER(pMarket)='INDONESIA' THEN B.nett_value ELSE B.nett_value_wna END FROM TEMP003 A
-       INNER JOIN MST022 B ON A.mst022_id = B.ID
-       INNER JOIN MST023 C ON C.ID = A.mst023_id
+       SELECT B.mst020_id, C.room_name, B.num_adults, B.num_child, B.num_breakfast,
+             B.from_date, B.end_date, B.net_fee, B.net, B.cancel_fee_flag, B.cancel_fee_val,
+             B.allotment-B.used_allotment AS allotment, B.comm_value, B.cut_off, B.bed_type,
+             CASE WHEN UPPER(pMarket)='INDONESIA' 
+             THEN B.nett_value 
+                ELSE B.nett_value_wna 
+             END as nett_value
+       FROM TEMP003 A
+       INNER JOIN MST022 B ON A.mst022_id = B.id
+       INNER JOIN MST023 C ON C.id = A.mst023_id
        WHERE A.mst023_id IN (SELECT B.mst023_id FROM TEMP004 B
-                              WHERE B.RECORD = (SELECT MAX(C.RECORD) FROM TEMP004 C
-                                                WHERE C.SESSION_ID = B.SESSION_ID)
-                              AND B.SESSION_ID = pSessionId)
-       AND A.SESSION_ID = pSessionId;
+                              WHERE B.record = (SELECT MAX(C.record) FROM TEMP004 C
+                                                WHERE C.session_id = B.session_id)
+                              AND B.session_id = pSessionId)
+       AND A.session_id = pSessionId;
 
-       DELETE FROM TEMP003 WHERE SESSION_ID = pSessionId;
-       DELETE FROM TEMP004 WHERE SESSION_ID = pSessionId;
+       DELETE FROM TEMP003 WHERE session_id = pSessionId;
+       DELETE FROM TEMP004 WHERE session_id = pSessionId;
 END;
