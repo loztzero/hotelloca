51721875ac5a28bd88595492c65cff412171bb72
loztzero <support@istoreline.com>
2016-06-30 19:20:36 +0700

    benerin query cancel otomatis

--- document/UpdateCancelOtomatis.sql
+++ document/UpdateCancelOtomatis.sql
@@ -1,16 +1,33 @@
-INSERT INTO LOG020 (
-	id,mst023_id,check_in_date,allotment,used_allotment,updated_at,created_at)
-SELECT
-	A.ID,B.mst023_id,A.check_in_date,D.allotment,B.room_num*-1,NOW(),NOW()
-FROM BLNC004 A 
-INNER JOIN BLNC002 B ON B.id = A.blnc002_id
-INNER JOIN BLNC001 C ON C.id = B.blnc001_id
-INNER JOIN BLNC003 E ON E.blnc001_id = C.id	
-INNER JOIN MST022 D ON D.mst023_id = B.mst023_id
-						  AND A.check_in_date BETWEEN D.from_date AND D.end_date
-WHERE  DATE_ADD(E.created_at,INTERVAL 1 HOUR) < NOW()
-AND E.conf_payment_date is null
-AND E.payment_method = 'Transfer';
+update LOG020 set used_allotment = used_allotment - (SELECT SUM(B.room_num)
+												FROM BLNC004 A 
+												INNER JOIN BLNC002 B ON B.id = A.blnc002_id
+												INNER JOIN BLNC001 C ON C.id = B.blnc001_id
+												INNER JOIN BLNC003 E ON E.blnc001_id = C.id	
+												INNER JOIN MST022 D ON D.mst023_id = B.mst023_id
+						  												  AND A.check_in_date 
+																		  BETWEEN D.from_date AND D.end_date
+												WHERE  DATE_ADD(E.created_at,INTERVAL 1 HOUR) < NOW()
+												AND E.conf_payment_date is null
+												AND E.payment_method = 'Transfer'
+												AND LOG020.mst023_id = B.mst023_id
+												AND LOG020.check_in_date = A.check_in_date )
+WHERE EXISTS (SELECT 1
+				  FROM BLNC004 A 
+				  INNER JOIN BLNC002 B ON B.id = A.blnc002_id
+				  INNER JOIN BLNC001 C ON C.id = B.blnc001_id
+				  INNER JOIN BLNC003 E ON E.blnc001_id = C.id	
+				  INNER JOIN MST022 D ON D.mst023_id = B.mst023_id
+				   						  AND A.check_in_date 
+											  BETWEEN D.from_date AND D.end_date
+				  WHERE  DATE_ADD(E.created_at,INTERVAL 1 HOUR) < NOW()
+				  AND E.conf_payment_date is null
+				  AND E.payment_method = 'Transfer'
+				  AND LOG020.mst023_id = B.mst023_id
+				  AND LOG020.check_in_date = A.check_in_date);
+
+
+
+
 
 
 UPDATE BLNC001 SET status_flag = 'Cancel',status_pymnt = 'Failed'
